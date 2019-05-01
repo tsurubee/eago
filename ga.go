dfb116249623fd62bf908b1d8a4318c59f39725d
tsurubee <29603512+tsurubee@users.noreply.github.com>
2019-05-01 16:32:08 +0900

    Implemented initialize population

--- ga.go
+++ ga.go
@@ -1,1 +1,46 @@
 package eago
+
+import "fmt"
+
+type GA struct {
+	GAConfig    *GAConfig
+	Population  Population
+	Generations uint
+	Best        Individual
+}
+
+type GAConfig struct {
+	PopulationSize uint
+	NGenerations   uint
+	MutationRate   float64
+}
+
+type Individual struct {
+	Chromosome Genome
+	Fitness    float64
+}
+
+type Population []Individual
+
+func NewGA() *GA {
+	return &GA{
+		GAConfig: &GAConfig{
+			PopulationSize: 10,
+			NGenerations:   10,
+			MutationRate:   0.05,
+		},
+	}
+}
+
+func (ga *GA) initPopulation(g Genome) Population {
+	pop := make(Population, ga.GAConfig.PopulationSize)
+	for i := range pop {
+		pop[i].Chromosome = g.Initialization()
+	}
+	return pop
+}
+
+func (ga *GA) Run(g Genome) {
+	pop := ga.initPopulation(g)
+	fmt.Println(pop)
+}
