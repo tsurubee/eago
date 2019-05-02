be6f4f78a3d3838b3ed4f9bb3596f07317bcfea7
tsurubee <29603512+tsurubee@users.noreply.github.com>
2019-05-02 22:14:24 +0900

    Change sample to Ackley function

--- examples/ga/example.go
+++ examples/ga/example.go
@@ -3,16 +3,17 @@ package main
 import (
 	"github.com/tsurubee/eago"
 	"log"
+	"math"
 )
 
 type Variables []float64
 
 func (V Variables) Initialization() eago.Genome {
-	return Variables(eago.InitFloatVector(2, 20, -20))
+	return Variables(eago.InitFloatVector(2, 32, -32))
 }
 
 func (V Variables) Fitness() float64 {
-	return V[0] * V[0] + V[1] * V[1]
+	return Ackley(V)
 }
 
 func (V Variables) Mutation() {
@@ -21,6 +22,16 @@ func (V Variables) Mutation() {
 
 func (V Variables) Crossover(X eago.Genome) eago.Genome {
 	return Variables(eago.BLXalpha(V, X.(Variables), 0.3))
+}
+
+func Ackley(X []float64) float64 {
+	a, b, c, d := 20.0, 0.2, 2*math.Pi, float64(len(X))
+	var s1, s2 float64
+	for _, x := range X {
+		s1 += x * x
+		s2 += math.Cos(c * x)
+	}
+	return -a*math.Exp(-b*math.Sqrt(s1/d)) - math.Exp(s2/d) + a + math.Exp(1)
 }
 
 func main() {
