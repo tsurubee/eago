902437d9fee7aa70d6426be5222f6686b0320f5c
tsurubee <29603512+tsurubee@users.noreply.github.com>
2019-05-02 15:29:51 +0900

    Add tournament selection

--- selection.go
+++ selection.go
@@ -1,13 +1,35 @@
 package eago
 
+import (
+	"errors"
+	"time"
+	"math/rand"
+)
+
 type Selector interface {
-	Select(indi Individuals) Individuals
+	Select(indi Individuals) (Individuals, error)
 }
 
 type Tournament struct {
-	NSelection uint
+	NSelection int
 }
 
-func (t Tournament) Select(indi Individuals) Individuals {
+func (t Tournament) Select(indi Individuals) (Individuals, error) {
+	if len(indi) < t.NSelection {
+		return nil, errors.New("invalid NSelection: Too large NSelection")
+	}
+	selected := make(Individuals, len(indi))
+	rand.Seed(time.Now().UnixNano())
+	for i := range selected {
+		winner := indi[rand.Intn(len(indi))]
+		for j := 0; j < t.NSelection; j++ {
+			next := indi[rand.Intn(len(indi))]
+			if winner.Fitness > next.Fitness {
+				winner = next
+			}
+		}
+		selected[i] = winner
+	}
 
+	return selected, nil
 }
